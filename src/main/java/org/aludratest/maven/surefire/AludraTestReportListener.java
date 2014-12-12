670a338ef853e996159580cd8aee5ea88f5e3a33
Florian Albrecht <Florian.Albrecht@hamburgsud.com>
2014-12-12 09:54:52 +0100

    Also take error message if status is same
    This allows e.g. SystemConnector errors to be the Surefire error.

--- src/main/java/org/aludratest/maven/surefire/AludraTestReportListener.java
+++ src/main/java/org/aludratest/maven/surefire/AludraTestReportListener.java
@@ -39,205 +39,205 @@ import org.apache.maven.surefire.util.ReflectionUtils;
  */
 public class AludraTestReportListener implements InvocationHandler {
 
-	private AtomicInteger testsRun = new AtomicInteger();
+    private AtomicInteger testsRun = new AtomicInteger();
 
-	private AtomicInteger testsSkipped = new AtomicInteger();
+    private AtomicInteger testsSkipped = new AtomicInteger();
 
-	private AtomicInteger testsFailed = new AtomicInteger();
+    private AtomicInteger testsFailed = new AtomicInteger();
 
-	private AtomicInteger testErrors = new AtomicInteger();
+    private AtomicInteger testErrors = new AtomicInteger();
 
-	private Map<Object, TestStatus> testStates = new HashMap<Object, TestStatus>();
+    private Map<Object, TestStatus> testStates = new HashMap<Object, TestStatus>();
 
-	private Map<Object, Long> testStartTimes = new HashMap<Object, Long>();
+    private Map<Object, Long> testStartTimes = new HashMap<Object, Long>();
 
-	private Map<Object, Throwable> testThrowables = new HashMap<Object, Throwable>();
+    private Map<Object, Throwable> testThrowables = new HashMap<Object, Throwable>();
 
-	private RunListener reporter;
+    private RunListener reporter;
 
-	public AludraTestReportListener(RunListener reporter) {
-		this.reporter = reporter;
-	}
+    public AludraTestReportListener(RunListener reporter) {
+        this.reporter = reporter;
+    }
 
-	@Override
-	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		String methodName = method.getName();
-		
-		if ("startingTestProcess".equals(methodName)) {
-			handleStartingTestProcess(args[0]);
-		}
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        String methodName = method.getName();
 
-		if ("startingTestGroup".equals(methodName)) {
-			handleStartingRunnerGroup(args[0]);
-		}
-		if ("startingTestLeaf".equals(methodName)) {
-			handleStartingRunnerLeaf(args[0]);
-		}
-		if ("finishedTestLeaf".equals(methodName)) {
-			handleFinishedRunnerLeaf(args[0]);
-		}
-		if ("finishedTestGroup".equals(methodName)) {
-			handleFinishedRunnerGroup(args[0]);
-		}
-		if ("finishedTestProcess".equals(methodName)) {
-			handleFinishedTestProcess(args[0]);
-		}
+        if ("startingTestProcess".equals(methodName)) {
+            handleStartingTestProcess(args[0]);
+        }
 
-		if ("newTestStep".equals(methodName)) {
-			handleTestStep(args[0], args[1]);
-		}
+        if ("startingTestGroup".equals(methodName)) {
+            handleStartingRunnerGroup(args[0]);
+        }
+        if ("startingTestLeaf".equals(methodName)) {
+            handleStartingRunnerLeaf(args[0]);
+        }
+        if ("finishedTestLeaf".equals(methodName)) {
+            handleFinishedRunnerLeaf(args[0]);
+        }
+        if ("finishedTestGroup".equals(methodName)) {
+            handleFinishedRunnerGroup(args[0]);
+        }
+        if ("finishedTestProcess".equals(methodName)) {
+            handleFinishedTestProcess(args[0]);
+        }
 
-		// equals must be implemented, for adding to listener registry
-		if ("equals".equals(methodName)) {
-			return args[0] == proxy;
-		}
+        if ("newTestStep".equals(methodName)) {
+            handleTestStep(args[0], args[1]);
+        }
 
-		// it's only a "listener" interface, no need to delegate
-		return null;
-	}
+        // equals must be implemented, for adding to listener registry
+        if ("equals".equals(methodName)) {
+            return args[0] == proxy;
+        }
 
-	private void extractTestState(Object runnerLeaf, Object testStepInfo) throws Exception {
+        // it's only a "listener" interface, no need to delegate
+        return null;
+    }
 
-		Object objStatus = testStepInfo.getClass().getMethod("getTestStatus").invoke(testStepInfo);
+    private void extractTestState(Object runnerLeaf, Object testStepInfo) throws Exception {
 
-		// convert to our class loader
-		TestStatus status = TestStatus.valueOf(objStatus.toString());
+        Object objStatus = testStepInfo.getClass().getMethod("getTestStatus").invoke(testStepInfo);
 
-		// only set if it's "worse" than the current one
-		TestStatus prevStatus = testStates.get(runnerLeaf);
+        // convert to our class loader
+        TestStatus status = TestStatus.valueOf(objStatus.toString());
 
-		if (isWorseThan(status, prevStatus)) {
-			testStates.put(runnerLeaf, status);
-		}
-	}
+        // only set if it's "worse" than the current one, or equal state
+        TestStatus prevStatus = testStates.get(runnerLeaf);
 
-	private boolean isWorseThan(TestStatus status1, TestStatus status2) {
-		if (status2 == null || status2 == TestStatus.IGNORED) {
-			return true;
-		}
+        if (isWorseThanOrEqual(status, prevStatus)) {
+            testStates.put(runnerLeaf, status);
+        }
+    }
 
-		if (status2 == TestStatus.PASSED) {
-			return status1.isFailure();
-		}
+    private boolean isWorseThanOrEqual(TestStatus status1, TestStatus status2) {
+        if (status2 == null || status2 == TestStatus.IGNORED || status1 == status2) {
+            return true;
+        }
 
-		if (status2 == TestStatus.FAILED || status2 == TestStatus.FAILEDPERFORMANCE) {
-			return status1.isFailure() && (status1 != TestStatus.FAILED && status1 != TestStatus.FAILEDPERFORMANCE);
-		}
+        if (status2 == TestStatus.PASSED) {
+            return status1.isFailure();
+        }
 
-		return false;
-	}
+        if (status2 == TestStatus.FAILED || status2 == TestStatus.FAILEDPERFORMANCE) {
+            return status1.isFailure() && (status1 != TestStatus.FAILED && status1 != TestStatus.FAILEDPERFORMANCE);
+        }
 
-	public RunResult createRunResult() {
-		return new RunResult(testsRun.get(), testErrors.get(), testsFailed.get(), testsSkipped.get());
-	}
+        return false;
+    }
 
-	private String getSourceName(Object runnerLeaf) {
-		return AludraTestReflectionUtil.getTestClassName(runnerLeaf);
-	}
+    public RunResult createRunResult() {
+        return new RunResult(testsRun.get(), testErrors.get(), testsFailed.get(), testsSkipped.get());
+    }
 
-	private String getTestName(Object runnerLeaf) {
-		return AludraTestReflectionUtil.getName(runnerLeaf);
-	}
+    private String getSourceName(Object runnerLeaf) {
+        return AludraTestReflectionUtil.getTestClassName(runnerLeaf);
+    }
 
-	private void handleStartingTestProcess(Object runnerTree) {
-		// currently, nothing to do
-	}
+    private String getTestName(Object runnerLeaf) {
+        return AludraTestReflectionUtil.getName(runnerLeaf);
+    }
 
-	private void handleStartingRunnerGroup(Object runnerGroup) {
-		// only handle if group contains leafs
-		if (AludraTestReflectionUtil.groupContainsLeafs(runnerGroup)) {
-			ReportEntry entry = new SimpleReportEntry(AludraTestReflectionUtil.getParentName(runnerGroup),
-					AludraTestReflectionUtil.getName(runnerGroup));
-			reporter.testSetStarting(entry);
-		}
-	}
+    private void handleStartingTestProcess(Object runnerTree) {
+        // currently, nothing to do
+    }
 
-	private void handleStartingRunnerLeaf(Object runnerLeaf) {
-		testStartTimes.put(runnerLeaf, System.currentTimeMillis());
+    private void handleStartingRunnerGroup(Object runnerGroup) {
+        // only handle if group contains leafs
+        if (AludraTestReflectionUtil.groupContainsLeafs(runnerGroup)) {
+            ReportEntry entry = new SimpleReportEntry(AludraTestReflectionUtil.getParentName(runnerGroup),
+                    AludraTestReflectionUtil.getName(runnerGroup));
+            reporter.testSetStarting(entry);
+        }
+    }
 
-		// extract "ignored" attribute from leaf
-		boolean ignored = AludraTestReflectionUtil.isIgnored(runnerLeaf);
-		if (ignored) {
-			return;
-		}
+    private void handleStartingRunnerLeaf(Object runnerLeaf) {
+        testStartTimes.put(runnerLeaf, System.currentTimeMillis());
 
-		// TODO add group here as soon as AludraTest supports it (e.g. "Approved / In Work" etc.)
-		ReportEntry entry = new CategorizedReportEntry(getSourceName(runnerLeaf), getTestName(runnerLeaf), null);
+        // extract "ignored" attribute from leaf
+        boolean ignored = AludraTestReflectionUtil.isIgnored(runnerLeaf);
+        if (ignored) {
+            return;
+        }
 
-		reporter.testStarting(entry);
-	}
+        // TODO add group here as soon as AludraTest supports it (e.g. "Approved / In Work" etc.)
+        ReportEntry entry = new CategorizedReportEntry(getSourceName(runnerLeaf), getTestName(runnerLeaf), null);
 
-	private void handleFinishedRunnerLeaf(Object runnerLeaf) {
-		testsRun.incrementAndGet();
+        reporter.testStarting(entry);
+    }
 
-		ReportEntry entry;
-		
-		String sourceName = getSourceName(runnerLeaf);
-		String name = getTestName(runnerLeaf);
-		
-		StackTraceWriter stackTrace = null;
-		Throwable t = testThrowables.get(runnerLeaf);
-		if (t != null) {
-			stackTrace = new PojoStackTraceWriter(sourceName, name, t);
-		}
+    private void handleFinishedRunnerLeaf(Object runnerLeaf) {
+        testsRun.incrementAndGet();
 
-		// get test state
-		TestStatus status = testStates.get(runnerLeaf);
-		if (AludraTestReflectionUtil.isIgnored(runnerLeaf)) {
-			status = TestStatus.IGNORED;
-		}
-		if (status == null) {
-			status = TestStatus.PASSED;
-		}
+        ReportEntry entry;
 
-		if (status.isFailure()) {
-			entry = CategorizedReportEntry.withException(sourceName, name, stackTrace);
-			if (status == TestStatus.FAILED || status == TestStatus.FAILEDPERFORMANCE) {
-				testsFailed.incrementAndGet();
-				reporter.testFailed(entry);
-			}
-			else {
-				testErrors.incrementAndGet();
-				reporter.testError(entry);
-			}
-		}
-		else {
-			Long startTime = testStartTimes.get(runnerLeaf);
-			Integer elapsed = null;
-			if (startTime != null) {
-				elapsed = Long.valueOf(System.currentTimeMillis() - startTime.longValue()).intValue();
-			}
-			entry = new CategorizedReportEntry(sourceName, sourceName, name, null, elapsed);
-			if (status == TestStatus.IGNORED) {
-				reporter.testSkipped(entry);
-			}
-			else {
-				reporter.testSucceeded(entry);
-			}
-		}
-	}
+        String sourceName = getSourceName(runnerLeaf);
+        String name = getTestName(runnerLeaf);
 
-	private void handleFinishedRunnerGroup(Object runnerGroup) {
-		if (AludraTestReflectionUtil.groupContainsLeafs(runnerGroup)) {
-			ReportEntry entry = new SimpleReportEntry(AludraTestReflectionUtil.getParentName(runnerGroup),
-					AludraTestReflectionUtil.getName(runnerGroup));
-			reporter.testSetCompleted(entry);
-		}
-	}
+        StackTraceWriter stackTrace = null;
+        Throwable t = testThrowables.get(runnerLeaf);
+        if (t != null) {
+            stackTrace = new PojoStackTraceWriter(sourceName, name, t);
+        }
 
-	private void handleFinishedTestProcess(Object runnerTree) {
-		// currently, nothing to do.
-	}
+        // get test state
+        TestStatus status = testStates.get(runnerLeaf);
+        if (AludraTestReflectionUtil.isIgnored(runnerLeaf)) {
+            status = TestStatus.IGNORED;
+        }
+        if (status == null) {
+            status = TestStatus.PASSED;
+        }
 
-	private void handleTestStep(Object runnerLeaf, Object testStep) throws Exception {
-		// extract test status from step
-		extractTestState(runnerLeaf, testStep);
+        if (status.isFailure()) {
+            entry = CategorizedReportEntry.withException(sourceName, name, stackTrace);
+            if (status == TestStatus.FAILED || status == TestStatus.FAILEDPERFORMANCE) {
+                testsFailed.incrementAndGet();
+                reporter.testFailed(entry);
+            }
+            else {
+                testErrors.incrementAndGet();
+                reporter.testError(entry);
+            }
+        }
+        else {
+            Long startTime = testStartTimes.get(runnerLeaf);
+            Integer elapsed = null;
+            if (startTime != null) {
+                elapsed = Long.valueOf(System.currentTimeMillis() - startTime.longValue()).intValue();
+            }
+            entry = new CategorizedReportEntry(sourceName, sourceName, name, null, elapsed);
+            if (status == TestStatus.IGNORED) {
+                reporter.testSkipped(entry);
+            }
+            else {
+                reporter.testSucceeded(entry);
+            }
+        }
+    }
 
-		// if there is a Throwable attached, store it
-		Throwable t = (Throwable) ReflectionUtils.invokeGetter(testStep, "getError");
-		if (t != null) {
-			testThrowables.put(runnerLeaf, t);
-		}
-	}
+    private void handleFinishedRunnerGroup(Object runnerGroup) {
+        if (AludraTestReflectionUtil.groupContainsLeafs(runnerGroup)) {
+            ReportEntry entry = new SimpleReportEntry(AludraTestReflectionUtil.getParentName(runnerGroup),
+                    AludraTestReflectionUtil.getName(runnerGroup));
+            reporter.testSetCompleted(entry);
+        }
+    }
+
+    private void handleFinishedTestProcess(Object runnerTree) {
+        // currently, nothing to do.
+    }
+
+    private void handleTestStep(Object runnerLeaf, Object testStep) throws Exception {
+        // extract test status from step
+        extractTestState(runnerLeaf, testStep);
+
+        // if there is a Throwable attached, store it
+        Throwable t = (Throwable) ReflectionUtils.invokeGetter(testStep, "getError");
+        if (t != null) {
+            testThrowables.put(runnerLeaf, t);
+        }
+    }
 
 }
